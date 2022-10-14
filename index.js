c24b2309eee9064b33062b74fc9fbca24cda8145
Shayn Morgan <shayn-morgan@hotmail.com>
2022-10-14 07:19:44 -0400

    play command added

--- index.js
+++ index.js
@@ -54,3 +54,21 @@ if(LOAD_SLASH) {
         }
     })
 }
+else{
+    client.on("ready", () => {
+        console.log(`Logged in as ${client.user.tag}`)
+    })
+    client.on("interactionCreate", (interaction) => {
+       async function handleCommand(){
+        if(!interaction.isCommand()) return
+
+        const slashcmd = client.slashcommands.get(interaction.commandName)
+        if(!slashcmd) interaction.reply("Not a valid slash command")
+
+        await interaction.deferReply()
+        await slashcmd.run({client, interaction})
+       } 
+       handleCommand()
+    })
+    client.login(TOKEN)
+}
