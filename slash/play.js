c1703e6bab2201241faa5aaa9cd416f42267e247
Daniel Viola <daniel.viola001@mymdc.net>
2022-10-14 17:36:48 -0400

    Removed node modules from being pushed and env, and made minor spacing adjustments on the files

--- slash/play.js
+++ slash/play.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder} = require("@discordjs/builders")
+const { SlashCommandBuilder } = require("@discordjs/builders")
 const { MessageEmbed } = require("discord.js")
 const { QueryType } = require("discord-player")
 
@@ -6,80 +6,80 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName("play")
         .setDescription("Loads songs from youtube")
-        .addSubcommand((subcommand) => 
+        .addSubcommand((subcommand) =>
             subcommand
                 .setName("song")
                 .setDescription("Loads a single song from a url")
                 .addStringOption((option) => option.setName("url").setDescription("The song's url").setRequired(true))
-         )
-         .addSubcommand((subcommand) => 
+        )
+        .addSubcommand((subcommand) =>
             subcommand
                 .setName("playlist")
                 .setDescription("Loads a playlist of songs from a url")
                 .addStringOption((option) => option.setName("url").setDescription("The playlist's url").setRequired(true))
-         )
-         .addSubcommand((subcommand) =>
+        )
+        .addSubcommand((subcommand) =>
             subcommand.setName("search").setDescription("Searches for the song based on provided keywords")
-            .addStringOption((option) => option.setName("searchterms").setDescription("The search keywords").setRequired(true)
-         )
-    ),
-    run: async ({ client, interaction}) => {
-        if(!interaction.member.voice.channel)
+                .addStringOption((option) => option.setName("searchterms").setDescription("The search keywords").setRequired(true)
+                )
+        ),
+    run: async ({ client, interaction }) => {
+        if (!interaction.member.voice.channel)
             return interaction.editReply("You need to be in a VC to use this command")
-        
+
         const queue = await client.player.createQueue(interaction.guild)
-        if(!queue.connection) await queue.connect(interaction.member.voice.channel)
+        if (!queue.connection) await queue.connect(interaction.member.voice.channel)
 
         let embed = new MessageEmbed()
 
-        if(interaction.options.getSubcommand() === "song"){
+        if (interaction.options.getSubcommand() === "song") {
             let url = interaction.options.getString("url")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
                 searchEngine: QueryType.YOUTUBE_VIDEO
             })
-            if(result.tracks.length === 0)
+            if (result.tracks.length === 0)
                 return interaction.editReply("No results")
-            
-                const song = result.tracks[0]
-                await queue.addTrack(song)
-                embed
-                    .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
-                    .setThumbnail(song.thumbnail)
-                    .setFooter({ text: `Duration: ${song.duration}`})
 
-        } else if(interaction.options.getSubcommand() === "playlist"){
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}` })
+
+        } else if (interaction.options.getSubcommand() === "playlist") {
             let url = interaction.options.getString("url")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
                 searchEngine: QueryType.YOUTUBE_PLAYLIST
             })
-            if(result.tracks.length === 0)
+            if (result.tracks.length === 0)
                 return interaction.editReply("No results")
-            
-                const playlist = result.playlist
-                await queue.addTracks(result.tracks)
-                embed
-                    .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** has been added to the Queue`)
-                    .setThumbnail(playlist.thumbnail)
 
-        } else if(interaction.options.getSubcommand() === "search"){
+            const playlist = result.playlist
+            await queue.addTracks(result.tracks)
+            embed
+                .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** has been added to the Queue`)
+                .setThumbnail(playlist.thumbnail)
+
+        } else if (interaction.options.getSubcommand() === "search") {
             let url = interaction.options.getString("searchterms")
             const result = await client.player.search(url, {
                 requestedBy: interaction.user,
                 searchEngine: QueryType.AUTO
             })
-            if(result.tracks.length === 0)
+            if (result.tracks.length === 0)
                 return interaction.editReply("No results")
-            
-                const song = result.tracks[0]
-                await queue.addTrack(song)
-                embed
-                    .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
-                    .setThumbnail(song.thumbnail)
-                    .setFooter({ text: `Duration: ${song.duration}`})
+
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}` })
         }
-        if(!queue.playing) await queue.play()
+        if (!queue.playing) await queue.play()
         await interaction.editReply({
             embeds: [embed]
         })
