5faff8fa244793ed61fe407dc727de9ab2578324
Shayn Morgan <shayn-morgan@hotmail.com>
2022-10-14 19:21:01 -0400

    fix issue with play

--- slash/play.js
+++ slash/play.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder} = require("@discordjs/builders")
-const { MessageEmbed } = require("discord.js")
+const { EmbedBuilder } = require("discord.js")
 const { QueryType } = require("discord-player")
 
 module.exports = {
@@ -19,8 +19,10 @@ module.exports = {
                 .addStringOption((option) => option.setName("url").setDescription("The playlist's url").setRequired(true))
          )
          .addSubcommand((subcommand) =>
-            subcommand.setName("search").setDescription("Searches for the song based on provided keywords")
-            .addStringOption((option) => option.setName("searchterms").setDescription("The search keywords").setRequired(true)
+            subcommand
+                .setName("search")
+                .setDescription("Searches for the song based on provided keywords")
+                .addStringOption((option) => option.setName("searchterms").setDescription("The search keywords").setRequired(true)
          )
     ),
     run: async ({ client, interaction}) => {
@@ -30,7 +32,7 @@ module.exports = {
         const queue = await client.player.createQueue(interaction.guild)
         if(!queue.connection) await queue.connect(interaction.member.voice.channel)
 
-        let embed = new MessageEmbed()
+        let embed = new EmbedBuilder()
 
         if(interaction.options.getSubcommand() === "song"){
             let url = interaction.options.getString("url")
@@ -41,12 +43,12 @@ module.exports = {
             if(result.tracks.length === 0)
                 return interaction.editReply("No results")
             
-                const song = result.tracks[0]
-                await queue.addTrack(song)
-                embed
-                    .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
-                    .setThumbnail(song.thumbnail)
-                    .setFooter({ text: `Duration: ${song.duration}`})
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
 
         } else if(interaction.options.getSubcommand() === "playlist"){
             let url = interaction.options.getString("url")
@@ -57,11 +59,11 @@ module.exports = {
             if(result.tracks.length === 0)
                 return interaction.editReply("No results")
             
-                const playlist = result.playlist
-                await queue.addTracks(result.tracks)
-                embed
-                    .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** has been added to the Queue`)
-                    .setThumbnail(playlist.thumbnail)
+            const playlist = result.playlist
+            await queue.addTracks(result.tracks)
+            embed
+                .setDescription(`**${result.tracks.length} songs from [${playlist.title}](${playlist.url})** has been added to the Queue`)
+                .setThumbnail(playlist.thumbnail)
 
         } else if(interaction.options.getSubcommand() === "search"){
             let url = interaction.options.getString("searchterms")
@@ -72,12 +74,12 @@ module.exports = {
             if(result.tracks.length === 0)
                 return interaction.editReply("No results")
             
-                const song = result.tracks[0]
-                await queue.addTrack(song)
-                embed
-                    .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
-                    .setThumbnail(song.thumbnail)
-                    .setFooter({ text: `Duration: ${song.duration}`})
+            const song = result.tracks[0]
+            await queue.addTrack(song)
+            embed
+                .setDescription(`**[${song.title}](${song.url})** has been added to the Queue`)
+                .setThumbnail(song.thumbnail)
+                .setFooter({ text: `Duration: ${song.duration}`})
         }
         if(!queue.playing) await queue.play()
         await interaction.editReply({
